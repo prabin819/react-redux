757f96095f67c4da7b1f2aff0bb4051a6403e72e
Prabin <manandharprabin819@gmail.com>
2024-06-11 17:25:39 +0545

    react-redux app done without bindActionCreators vid 78

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+//import { bindActionCreators } from "redux";
+import { actionCreaters } from "../State";
 
 const Shop = () => {
+    const dispatch = useDispatch();
   return (
     <div>
       <h2>Deposit/Withdraw Money</h2>
-      <button className="btn btn-primary mx-2">-</button>
+      <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreaters.withdrawMoney(50))}}>-</button>
       Update Balance
-      <button className="btn btn-primary mx-2">+</button>
+      <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreaters.depositMoney(50))}}>+</button>
     </div>
   );
 };
